88a8f771714e737fa6bc71be5df92fa38816d44e
emilyhwng <emilyhwang816@gmail.com>
2023-04-27 23:32:49 -0700

    helloooo

--- crochet.py
+++ crochet.py
@@ -2,10 +2,11 @@ from PIL import Image
 import os
 
 os.system('cls')
-    
-def pixelate(image, max_pixels, colors):
-    image = Image.open(image)
 
+# maximizes dimensions maintaining original aspect ratio under the maximum number of pixels
+def get_dimensions(image, max_pixels):
+    image = Image.open(image)
+    
     width, height = image.size
     total_pixels = width * height
     if total_pixels > max_pixels:
@@ -14,24 +15,27 @@ def pixelate(image, max_pixels, colors):
         new_height = int(new_width / ratio)
     else:
         new_width, new_height = width, height
+    return new_width, new_height
 
-    image = image.resize((new_width, new_height))
-    
+# creates pixel grid using colors of palette represented by letters
+def pixelate(image, colors):
+    image = Image.open(image)
+
+    image = image.resize((width, height))
     palette = image.quantize(colors)
-    #image.show()
+    
     pattern = ''
-    for i in range(new_height):
-        for j in range(new_width):
+    for i in range(height):
+        for j in range(width):
             color_index = palette.getpixel((j, i))
             pattern += chr(ord('A') + color_index)
         pattern += '\n'
 
-    print()
-    print("Dimensions:", new_width, "x", new_height)
- 
+    print("\nDimensions:", width, "x", height)
     return pattern
 
-def consecutive_letters(pattern):
+# creates written pattern counting number of consecutives stitches in a color for each row, starting from the bottom left and working way up going back and forth
+def written_pattern(pattern):
     rows = pattern.split('\n')[:-1]
     result = []
     for row in rows:
@@ -61,12 +65,18 @@ def consecutive_letters(pattern):
         string = "row " + str(i + 1) + " " + side + ": " + result[i]
         print(string)
 
+# getting input information (file name, max pixels, and number of palette colors)
 name = input('what is the image file name? ')
 max_pixels = int(input('maximum number of pixels? '))
 colors = int(input('how many different colors do you want to use? '))
 file_name = name + '.jpg'
 
-crochet_pattern = pixelate(file_name, max_pixels, colors)
-print('\n' + crochet_pattern)
-counts = consecutive_letters(crochet_pattern)
-print()
+# calculating dimensions and making pixel grid
+width, height = get_dimensions(file_name, max_pixels)
+pixel_grid = pixelate(file_name, colors)
+
+# printing pixel grid and written pattern
+print('\n' + pixel_grid)
+print('---------- WRITTEN PATTERN ----------\n')
+print('foundation chain: ' + str(width) + ' chains')
+written_pattern(pixel_grid)
